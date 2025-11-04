d98f10dbf886bc3a071c75ed5617102e679cb183
Nick Lobanov <nickol.lobanov@gmail.com>
2025-11-04 23:57:47 +0300

    feat: vue-ls and config reduction

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -3,50 +3,10 @@ return {
   event = "LazyFile",
   opts = {
     servers = {
-      html = { enabled = true, format = { enable = false } },
-      cssls = { enabled = true },
-      angularls = { enabled = true },
-      eslint = { enabled = true },
       json = {
         filetypes = { "json" },
       },
-      -- ts_ls = {
-      --   enabled = true,
-      --   -- disable formatting by ts_ls in favor of eslint for example
-      --   on_init = function(client)
-      --     client.server_capabilities.documentFormattingProvider = false
-      --     client.server_capabilities.documentRangeFormattingProvider = false
-      --   end,
-      --   filetypes = { "typescript", "typescriptreact", "typescript.tsx", "angularhtml", "angular" },
-      --   settings = {
-      --     typescript = {
-      --       format = {
-      --         convertTabsToSpaces = true,
-      --       },
-      --       preferences = {
-      --         importModuleSpecifier = "shortest", -- New import style
-      --         jsxAttributeCompletionStyle = "auto", -- Smarter JSX
-      --       },
-      --       inlayHints = {
-      --         -- includeInlayParameterNameHints = "all",
-      --         includeInlayFunctionParameterTypeHints = true,
-      --       },
-      --       tsserver = {
-      --         experimental = {
-      --           enableProjectDiagnostics = true, -- Better monorepo support
-      --         },
-      --       },
-      --     },
-      --     javascript = {
-      --       preferences = {
-      --         importModuleSpecifier = "shortest",
-      --       },
-      --     },
-      --   },
-      --   root_markers = { "package.json", "tsconfig.json", "jsconfig.json", ".git" },
-      -- },
       vtsls = {
-        enabled = true,
         on_init = function(client)
           client.server_capabilities.documentFormattingProvider = false
           client.server_capabilities.documentRangeFormattingProvider = false
