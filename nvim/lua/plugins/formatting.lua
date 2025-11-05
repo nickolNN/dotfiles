119f8cce84fae3847d5a0b47cd710db81feec1fe
n.lobanov <n.lobanov@yadro.com>
2025-11-05 15:08:24 +0300

    chore: formatters config

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -3,8 +3,15 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { "stylua" },
-      typescript = { "eslint" },
-      html = { "angular", "eslint" },
+      typescript = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      javascript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      vue = { "eslint_d", "stylelint" },
+      go = { "gofmt", "goimports", "golines" },
+      html = { "eslint_d" },
+      css = { "stylelint" },
+      scss = { "stylelint" },
     },
   },
 }
--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -6,12 +6,16 @@ return {
       json = {
         filetypes = { "json" },
       },
-      vtsls = {
-        on_init = function(client)
-          client.server_capabilities.documentFormattingProvider = false
-          client.server_capabilities.documentRangeFormattingProvider = false
-        end,
+      angularls = {
+        filetypes = { "typescript", "html", "htmlangular" },
+        root_markers = { "angular.json" },
       },
+      -- vtsls = {
+      --   on_init = function(client)
+      --     client.server_capabilities.documentFormattingProvider = false
+      --     client.server_capabilities.documentRangeFormattingProvider = false
+      --   end,
+      -- },
     },
   },
 }
