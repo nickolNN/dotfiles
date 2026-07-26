57f3562fdcd69d062bd505c2a4b44dfca42ba398
Nick Lobanov <nickol.lobanov@gmail.com>
2026-07-26 14:26:53 +0300

    chore: flickering fix try

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,3 +4,8 @@
 local o = vim.o
 o.colorcolumn = "100"
 vim.opt.sessionoptions = "buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+-- Neovim's default terminal-mode cursor blinks (blinkon500-blinkoff500). In a
+-- :terminal panel (e.g. Kilo) that reads as a cursor blinking in sync with the
+-- TUI spinner. blinkon0/blinkoff0 makes it a solid block; other modes untouched.
+vim.opt.guicursor:append("t:block-blinkon0-blinkoff0-TermCursor")
